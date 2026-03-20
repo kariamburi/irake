27c5bf24151ae0284e10aa07c1b0b1065997c32b
Paul Irungu <paul.irungu@gmail.com>
2026-03-20 16:37:16 +0300

    first commit

---
+++ app/delete-account/page.tsx
@@ -0,0 +1,17 @@
+// app/delete-account/page.tsx
+
+export default function DeleteAccountPage() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Delete your ekarihub account</h1>
+      <p>
+        To request deletion of your account and all associated data,
+        please email us at support@ekarihub.com or use the in-app
+        Profile Edit settings under "Delete Account".
+      </p>
+      <p>
+        Once requested, your data will be permanently deleted within 7 days.
+      </p>
+    </div>
+  );
+}
